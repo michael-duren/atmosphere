530c6ab0af063a37b54a375812eb61d6c44a3696
Michael Duren <michaeld@michaelduren.com>
2023-07-30 07:27:20 -0500

    update to loading song, does not allow load if transport is started

--- client/src/components/Features/Daw/LoadSongWarning.tsx
+++ client/src/components/Features/Daw/LoadSongWarning.tsx
@@ -6,6 +6,7 @@ import { CURRENT_SONG_ACTIONS } from '../../../store/actions/currentSongActions.
 
 export default function LoadSongWarning() {
   const { songToLoad } = useAppSelector((store) => store.common);
+  const { isPlaying } = useAppSelector((store) => store.transport);
   const dispatch = useAppDispatch();
   const closeModal = () => dispatch(setLoadSongModalOpen(false));
   const loadSong = () => {
@@ -24,8 +25,19 @@ export default function LoadSongWarning() {
       <div className="flex w-full">
         <p>Any unsaved changes will be lost</p>
       </div>
+      {isPlaying && (
+        <div className="text-red-500 text-xs font-semibold shadow-xl">
+          Cannot Load Song while App is playing
+        </div>
+      )}
       <div className="flex w-full justify-around mt-8">
-        <ColorButton color="red" type="button" title="Yes" onClick={loadSong} />
+        <ColorButton
+          color="red"
+          type="button"
+          title="Yes"
+          disabled={isPlaying}
+          onClick={loadSong}
+        />
         <ColorButton
           color="green"
           type="button"
--- client/src/components/Ui/Buttons/ColorButton.tsx
+++ client/src/components/Ui/Buttons/ColorButton.tsx
@@ -6,6 +6,7 @@ interface Props {
   ItemIcon?: IconType;
   type: 'button' | 'submit' | 'reset' | undefined;
   color: string;
+  disabled?: boolean;
 }
 
 export default function ColorButton({
@@ -14,16 +15,20 @@ export default function ColorButton({
   ItemIcon,
   type,
   color,
+  disabled,
 }: Props) {
   return (
     <>
       {color === 'amber' ? (
         <button
           className={`inline-flex mt-4 w-24 justify-center opacity-90 shadow-md shadow-amber-950 rounded-xl border border-transparent bg-amber-900 px-4 py-2 
-      text-sm font-medium text-amber-100 hover:bg-amber-800 focus:outline-none focus-visible:ring-2 active:scale-105 focus-visible:ring-amber-500 
+      text-sm font-medium text-amber-100  focus:outline-none focus-visible:ring-2  focus-visible:ring-amber-500 ${
+        disabled ? '' : 'active:scale-105 hover:bg-amber-800'
+      }
       focus-visible:ring-offset-2`}
           onClick={onClick}
           type={type}
+          disabled={disabled}
         >
           {ItemIcon && <ItemIcon size={18} className="mr-2" />}
           <span>{title}</span>
@@ -31,10 +36,13 @@ export default function ColorButton({
       ) : color === 'green' ? (
         <button
           className={`inline-flex mt-4 w-24 justify-center opacity-90 shadow-md shadow-green-950 rounded-xl border border-transparent bg-green-900 px-4 py-2 
-      text-sm font-medium text-green-100 hover:bg-green-800 focus:outline-none focus-visible:ring-2 active:scale-105 focus-visible:ring-green-500 
+      text-sm font-medium text-green-100  focus:outline-none focus-visible:ring-2  focus-visible:ring-green-500 ${
+        disabled ? '' : 'active:scale-105 hover:bg-green-800'
+      }
       focus-visible:ring-offset-2`}
           onClick={onClick}
           type={type}
+          disabled={disabled}
         >
           {ItemIcon && <ItemIcon size={18} className="mr-2" />}
           <span>{title}</span>
@@ -42,10 +50,13 @@ export default function ColorButton({
       ) : color === 'blue' ? (
         <button
           className={`inline-flex mt-4 w-24 justify-center opacity-90 shadow-md shadow-blue-950 rounded-xl border border-transparent bg-blue-900 px-4 py-2 
-      text-sm font-medium text-blue-100 hover:bg-blue-800 focus:outline-none focus-visible:ring-2 active:scale-105 focus-visible:ring-blue-500 
+      text-sm font-medium text-blue-100  focus:outline-none focus-visible:ring-2  focus-visible:ring-blue-500 ${
+        disabled ? '' : 'active:scale-105 hover:bg-blue-800'
+      }
       focus-visible:ring-offset-2`}
           onClick={onClick}
           type={type}
+          disabled={disabled}
         >
           {ItemIcon && <ItemIcon size={18} className="mr-2" />}
           <span>{title}</span>
@@ -53,10 +64,13 @@ export default function ColorButton({
       ) : (
         <button
           className={`inline-flex mt-4 w-24 justify-center opacity-90 shadow-md shadow-red-950 rounded-xl border border-transparent bg-red-900 px-4 py-2 
-      text-sm font-medium text-red-100 hover:bg-red-800 focus:outline-none focus-visible:ring-2 active:scale-105 focus-visible:ring-red-500 
+      text-sm font-medium text-red-100  focus:outline-none focus-visible:ring-2  focus-visible:ring-red-500 ${
+        disabled ? '' : 'active:scale-105 hover:bg-red-800'
+      }
       focus-visible:ring-offset-2`}
           onClick={onClick}
           type={type}
+          disabled={disabled}
         >
           {ItemIcon && <ItemIcon size={18} className="mr-2" />}
           <span>{title}</span>
